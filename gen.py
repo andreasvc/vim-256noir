89b8d9f359a6d7732bd36556f1a2716f6f29f9ca
Andreas van Cranenburgh <andreas@unstable.nl>
2018-01-26 00:16:42 +0100

    improvements

--- gen.py
+++ gen.py
@@ -3,8 +3,13 @@
 Uses the tab-separated file 'noir.tsv' to generate a vim color scheme
 that works with 256 and 16 color terminals, as well as in gvim."""
 
-PREAMBLE="""\
-" 256 noir. Basically: dark background, numerals & errors red,
+PREAMBLE = """\
+" Vim color file
+" Name:       256_noir.vim
+" Maintainer: Andreas van Cranenburgh <andreas@unstable.nl>
+" Homepage:   https://github.com/andreasvc/vim-256noir/
+
+" Basically: dark background, numerals & errors red,
 " rest different shades of gray.
 "
 " colors 232--250 are shades of gray, from dark to light;
@@ -23,18 +28,21 @@ let g:colors_name = "256_noir"
 """
 
 TORGB = {
-        16: '000000',
-        88: '870000',
-        124: 'af0000',
-        160: 'd70000',
-        196: 'ff0000',
-        232: '080808',
-        236: '303030',
-        240: '585858',
-        245: '8a8a8a',
-        250: 'bcbcbc',
-        252: 'd0d0d0',
-        255: 'eeeeee',
+        16: '#000000',
+        88: '#870000',
+        124: '#af0000',
+        160: '#d70000',
+        196: '#ff0000',
+        232: '#080808',
+        233: '#121212',
+        234: '#1c1c1c',
+        236: '#303030',
+        240: '#585858',
+        245: '#8a8a8a',
+        250: '#bcbcbc',
+        252: '#d0d0d0',
+        255: '#eeeeee',
+        'NONE': 'NONE',
 }
 
 TO16 = {
@@ -44,22 +52,28 @@ TO16 = {
         160: 'Red',
         196: 'LightRed',
         232: 'Black',
+        233: 'Black',
+        234: 'DarkGray',
         236: 'DarkGray',
         240: 'DarkGray',
         245: 'LightGray',
         250: 'LightGray',
         252: 'LightGray',
         255: 'White',
+        'NONE': 'NONE',
 }
 
 if __name__ == '__main__':
     with open('noir.tsv', 'r') as inp:
-        table = [(key, int(fg), int(bg)) for key, fg, bg
+        table = [(key,
+                    'NONE' if fg == 'NONE' else int(fg),
+                    'NONE' if bg == 'NONE' else int(bg))
+                for key, fg, bg
                 in (line.strip().split() for line in inp.readlines()[1:])]
     print(PREAMBLE)
     print('if has("gui_running") || &t_Co == 256')
     for key, fg, bg in table:
-        print('\thi %s\tctermfg=%d\tctermbg=%d\tguifg=#%s\tguibg=#%s' % (
+        print('\thi %s\tctermfg=%s\tctermbg=%s\tguifg=%s\tguibg=%s' % (
                 key, fg, bg, TORGB[fg], TORGB[bg]))
     print('else')  # terminal with less colors, e.g., 88, 16, or 8.
     for key, fg, bg in table:
